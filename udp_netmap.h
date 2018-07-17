7b42d74d46da5ecef34aa316243457808b697dbc
Yeatol <vim@vimer.org>
2018-07-17 15:53:26 +0800

    update

--- udp_netmap.h
+++ udp_netmap.h
@@ -4,6 +4,9 @@
 using namespace std;
 
 void udp_config(const char* ifname, const char* gateway_ip);
+
 void udp_update(uint32_t current);
+
 void udp_input(uint32_t src_ip, uint16_t src_port, uint16_t dst_port, char* data, uint32_t size);
+
 void udp_output(uint32_t dst_ip, uint16_t dst_port, uint16_t src_port, void* data, uint32_t size);
